f33911bcc27ddd011a01e4e37ce3e07347a0d0a6
Dowon Kim <dowoniscool@yahoo.com>
2024-05-02 19:47:53 +0200

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,7 @@
 import { useState } from 'react'
-import { options, questionsMBTI } from './question'
+import { options, questionsMBTI } from './constants/questionsAndAnswers'
 import './App.css'
-
-function Question({question, options, onAnswer}) {
-  const [selectedOption, setSelectedOption] = useState('')
-
-  const handleNextQuestion = () => {
-    if (selectedOption == '그렇다.') {
-        onAnswer(question.YES)
-    } else {
-        onAnswer(question.No)
-    }
-    
-  }
-
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value)
-  }
-
-  return (
-    <>
-      <div>
-        <h2>{question.question}</h2>
-        <form>
-          {
-            options.map((option) => (
-              <div key={option}>
-                <label>
-                  <input
-                    type="radio"
-                    value={option}
-                    checked={selectedOption === option}
-                    onChange={handleOptionChange}
-                  />
-                  {option}
-                </label>
-              </div>
-            ))
-          }
-        </form>
-          <br />
-          <br />
-        <button onClick={handleNextQuestion}>Next</button>
-
-      </div>
-      
-    </>
-  )
-}
-
+import Question from './question'
 
 function App() {
   const [answers, setAnswers] = useState([])
